c3f69ea5bb3756b5c49de8bcf91a583e608a8984
campbell-mason <campbell.mason@hotmail.co.nz>
2023-04-27 10:36:26 +1200

    Mission 0 project

---
+++ M0/src/components/SearchBar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="searchbar">
+    <p>Welcome!! To My Website</p>
+    <div class="searchbar__box">
+      <input type="text" v-model="input" placeholder="Search teachers..." />
+      <button>Search</button>
+    </div>
+    <div class="music teacher" v-for="teacher in filteredList()" :key="teacher">
+      <p>{{ teacher }}</p>
+    </div>
+
+    <div class="name-error" v-if="input && !filteredList().length" :key="teacher">
+      <p>No results found !</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+let input = ref('')
+const musicTeacher = []
+function filteredList() {
+  return musicTeacher.filter((teachers) =>
+    teachers.toLowerCase().includes(input.value.toLowerCase())
+  )
+}
+</script>
+<style scoped>
+.searchbar {
+  height: 330px;
+  padding-top: 2%;
+  background-image: url('../assets/images/music.jpg');
+}
+.searchbar button {
+  width: 19.1%;
+  padding: 10px;
+  background: black;
+  color: white;
+  font-size: 15px;
+  border: 1px solid grey;
+  border-left: none; /* Prevent double borders */
+  cursor: pointer;
+}
+
+.searchbar input {
+  padding: 10px;
+  font-size: 15px;
+  width: 50%;
+  border: 1px solid grey;
+  float: left;
+  background: #f1f1f1;
+}
+
+.searchbar__box {
+  padding-left: 20%;
+}
+
+.searchbar p {
+  height: 70px;
+  width: 2500px;
+  padding-left: 20px;
+  padding-bottom: 100px;
+  color: azure;
+  font-size: 50px;
+}
+</style>
---
+++ M0/src/components/TeacherImage.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="cards">
+    <article class="card"> 
+        <img src="../assets/images/keyboard.jpg" alt="Hot air balloons">
+        <div class="content">
+            <header>Keyboard </header>
+            <p>hello</p>
+        </div>
+            
+    </article>
+            
+     <article class="card"> 
+        <img src="../assets/images/keyboard.jpg" alt="Hot air balloons">
+        <div class="content">
+            <header>Lopsem</header>
+            <p>hello</p>
+        </div>
+    </article>
+            
+    <article class="card">
+        <img src="../assets/images/keyboard.jpg" alt="Hot air balloons">
+        <div class="content">
+           <header>Lopsem</header>
+           <p>hello</p>
+        </div>
+
+    </article>
+</div>
+</template>
+<style scoped>
+.cards {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(3, 1fr);
+}
+
+.card {
+    display: grid;
+    padding: 30px 20px 1px 10px;
+
+    grid-template-rows: max-content 60px;
+}
+
+.card img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+.content {
+    background-color: gray;
+}
+.content header {
+   padding-left: 10px;
+}
+.content p {
+ padding: 0px 0px 10px 10px;
+}
+
+</style>
+
